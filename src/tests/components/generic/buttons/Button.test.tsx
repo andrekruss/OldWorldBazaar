c8987b386869859ee568376733ca4f4a66508ae3
leo-kruss <104536366+leo-kruss@users.noreply.github.com>
2026-07-29 18:51:50 -0300

    Teste onClick para componente Button

--- src/tests/components/generic/buttons/Button.test.tsx
+++ src/tests/components/generic/buttons/Button.test.tsx
@@ -1,9 +1,11 @@
-import {describe, expect, it} from "vitest";
+import {describe, expect, it, vi} from "vitest";
 import { render, screen } from "@testing-library/react";
 
 import Button from "../../../../modules/shared/generic/components/buttons/Button";
+import userEvent from "@testing-library/user-event";
 
 describe("Button", () => {
+
     it("renders its children", () => {
 
         // arrange
@@ -13,5 +15,26 @@ describe("Button", () => {
         expect(
             screen.getByRole("button", {name: "Register"})
         ).toBeInTheDocument();
+    });
+
+    it("calls onClick() when clicked", async () => {
+        
+        // arrange
+        const user = userEvent.setup();
+        const onClick = vi.fn();
+
+        render(
+            <Button onClick={onClick}>
+                Register
+            </Button>
+        );
+
+        // act
+        await user.click(
+            screen.getByRole("button", {name: "Register"})
+        );
+
+        // assert
+        expect(onClick).toHaveBeenCalledTimes(1);
     });
 });
